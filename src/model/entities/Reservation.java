cb2590855241d5dfd634eb704d8ae39521b31a55
Daniel Oliveira <daniel13.oliveira.do@gmail.com>
2020-03-28 15:36:33 -0300

    Bad solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -5,11 +5,11 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class Reservation {
-    
+
     private Integer roomNumber;
     private Date checkIn;
     private Date checkOut;
-    
+
     private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
     public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
@@ -33,24 +33,32 @@ public class Reservation {
     public Date getCheckOut() {
         return checkOut;
     }
-    
-    public long duration(){
+
+    public long duration() {
         long diff = checkOut.getTime() - checkIn.getTime();
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
-    
-    public void updateDates(Date checkIn, Date checkOut){
+
+    public String updateDates(Date checkIn, Date checkOut) {
+        Date now = new Date();
+        if (checkIn.before(now) || checkOut.before(now)) {
+            return "Error in reservation: Reservation datas for update must be future dates";
+        }
+        if (!checkOut.after(checkIn)) {
+            return "Error in reservation: Check-out date must be after check-in date";
+        }
         this.checkIn = checkIn;
         this.checkOut = checkOut;
+        return null;
     }
-    
+
     @Override
-    public String toString(){
-        return "Room " 
+    public String toString() {
+        return "Room "
                 + roomNumber
                 + ", check-in: "
                 + sdf.format(checkIn)
-                +", check-out: "
+                + ", check-out: "
                 + sdf.format(checkOut)
                 + ", "
                 + duration()
